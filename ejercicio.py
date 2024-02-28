f2896cdff86a24998a5472f1f220139c228eac87
hernandezmin <158304072+hernandezmin@users.noreply.github.com>
2024-02-28 08:55:25 -0600

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,16 @@
+N1=0
+N2=0
+op=" "
+Res=0
+N1=float(input("Ingrese un numero: "))
+N2=float(input("Ingrese otro numero: "))
+
+op=input("Que operacion dese realizar?: ")
+if op=="+":
+    print(N1+N2)
+if op=="-":
+    print(N1-N2)
+if op=="*":
+    print(N1*N2)
+if op=="/":
+    print(N1/N2)
